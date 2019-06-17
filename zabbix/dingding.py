e84ecbd8e605558cc605d047b67bc758d7b9efb2
huy <47073353+mykubernetes@users.noreply.github.com>
2019-06-17 11:11:44 +0800

    Update dingding.py

--- zabbix/dingding.py
+++ zabbix/dingding.py
@@ -6,19 +6,19 @@ import sys
 import os
   
 headers = {'Content-Type': 'application/json;charset=utf-8'}
-api_url = "https://oapi.dingtalk.com/robot/send?access_token=d4228bafa2a6ee69354752bc6e4a6a618c571610109a43115cbfc38b144596a0"
+api_url = "https://oapi.dingtalk.com/robot/send?access_token=d4228bafa2a6ee69354752bc6e4a6a618c571610109a43115cbfc38b144596a0"    #钉钉webhook
   
 def msg(text):
     json_text= {
      "msgtype": "text",
      "text": {
-         "content": text
+         "content": text                 #内容
      },
      "at": {
          "atMobiles": [
-             "186..."
+             "186..."                    #主动@的人
          ], 
-         "isAtAll": False
+         "isAtAll": False                #是否@所有人
      }
     }
     print requests.post(api_url,json.dumps(json_text),headers=headers).content
